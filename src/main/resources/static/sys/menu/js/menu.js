de98da1b57c021673bfb2bf93b541e283cb709f2
huanzi-qch <781792871@qq.com>
2020-08-10 10:01:35 +0800

    常见问题新增：如何逆向工程生成全套后端代码

--- src/main/resources/static/sys/menu/js/menu.js
+++ src/main/resources/static/sys/menu/js/menu.js
@@ -51,7 +51,7 @@ layui.use(['element', 'form', 'table', 'layer', 'tree', 'util'], function () {
                     //返回 key 值
                     return "";
                 } else if (type === 'del') { //删除节点
-                    layer.confirm('确认要删除这个菜单吗？/n注意：删除父节点将会一同删除子节点', function (index) {
+                    layer.confirm('确认要删除这个菜单吗？\n注意：删除父节点将会一同删除子节点', function (index) {
                         $.delete(ctx + "/sys/sysMenu/delete/" + data.id,{}, function () {
                             layer.msg("删除成功");
                             elem.remove();
