61920b58a221f35188ac2c1a2d25df83c8abbdbf
huanzi-qch <781792871@qq.com>
2020-08-20 15:21:07 +0800

    1、重写权限模块delete方法，先删与用户的关联权限表、更新权限集合，在删除自己；

--- src/main/java/cn/huanzi/qch/baseadmin/sys/sysauthority/service/SysAuthorityServiceImpl.java
+++ src/main/java/cn/huanzi/qch/baseadmin/sys/sysauthority/service/SysAuthorityServiceImpl.java
@@ -6,6 +6,8 @@ import cn.huanzi.qch.baseadmin.config.security.MyFilterInvocationSecurityMetadat
 import cn.huanzi.qch.baseadmin.sys.sysauthority.pojo.SysAuthority;
 import cn.huanzi.qch.baseadmin.sys.sysauthority.repository.SysAuthorityRepository;
 import cn.huanzi.qch.baseadmin.sys.sysauthority.vo.SysAuthorityVo;
+import cn.huanzi.qch.baseadmin.sys.sysuserauthority.service.SysUserAuthorityService;
+import cn.huanzi.qch.baseadmin.sys.sysuserauthority.vo.SysUserAuthorityVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,9 @@ public class SysAuthorityServiceImpl extends CommonServiceImpl<SysAuthorityVo, S
     private SysAuthorityService sysAuthorityService;
 
     @Autowired
+    private SysUserAuthorityService sysUserAuthorityService;
+
+    @Autowired
     private MyFilterInvocationSecurityMetadataSource myFilterInvocationSecurityMetadataSource;
 
     /**
@@ -35,6 +40,29 @@ public class SysAuthorityServiceImpl extends CommonServiceImpl<SysAuthorityVo, S
     @Override
     public Result<SysAuthorityVo> save(SysAuthorityVo entityVo) {
         Result<SysAuthorityVo> result = super.save(entityVo);
+
+        //更新权限集合
+        List<SysAuthorityVo> authorityVoList = sysAuthorityService.list(new SysAuthorityVo()).getData();
+        myFilterInvocationSecurityMetadataSource.setRequestMap(authorityVoList);
+        return result;
+    }
+
+    /**
+     * 重写delete方法
+     */
+    @Override
+    public Result<String> delete(String id) {
+        //删除权限之前，删除用户权限关联表对应数据
+        SysUserAuthorityVo sysUserAuthorityVo = new SysUserAuthorityVo();
+        sysUserAuthorityVo.setAuthorityId(id);
+        sysUserAuthorityService.list(sysUserAuthorityVo).getData().forEach((vo)->{
+            sysUserAuthorityService.delete(vo.getUserAuthorityId());
+        });
+
+        //再删除自己
+        Result<String> result = super.delete(id);
+
+        //更新权限集合
         List<SysAuthorityVo> authorityVoList = sysAuthorityService.list(new SysAuthorityVo()).getData();
         myFilterInvocationSecurityMetadataSource.setRequestMap(authorityVoList);
         return result;
