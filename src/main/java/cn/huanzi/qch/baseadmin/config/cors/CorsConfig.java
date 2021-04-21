16730338617cd9a93e5a51a54078ae0dba27fea5
huanzi-qch <781792871@qq.com>
2021-04-21 11:35:44 +0800

    1、新增cors安全跨域配置；

---
+++ src/main/java/cn/huanzi/qch/baseadmin/config/cors/CorsConfig.java
@@ -0,0 +1,31 @@
+package cn.huanzi.qch.baseadmin.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * cors安全跨域配置
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry
+                        //需要跨域的接口，例如：/openApi/*
+                        .addMapping("/openApi/*")
+                        //允许调用的域，例如：http://172.16.12.156:8888
+                        .allowedOrigins("*")
+                        //接口调用方式，POST、GET等
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
